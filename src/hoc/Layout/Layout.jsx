80c11af5e94974884d0dbce227c7dc839fcbf5cd
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-12-24 17:42:05 +0200

    Added side memu

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-import classes from './Layout.module.css';
+import Drawer from '../../components/ActiveQuiz/Navigation/MenuToggle/Drawer/Drawer';
+import MenuToggle from '../../components/ActiveQuiz/Navigation/MenuToggle/MenuToggle';
+import s from './Layout.module.css';
 
 class Layout extends Component {
+  state = {
+    menu: false,
+  };
+
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu,
+    });
+  };
   render() {
     return (
-      <div className={classes.Layout}>
+      <div className={s.Layout}>
+        <Drawer isOpen={this.state.menu} />
+        <MenuToggle
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.menu}
+        />
         <main>{this.props.children}</main>
       </div>
     );
