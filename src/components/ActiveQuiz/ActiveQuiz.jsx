452fa61c9295b73095ba2acf1c0573971fb2a46e
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-12-24 12:08:23 +0200

    Added next question

--- src/components/ActiveQuiz/ActiveQuiz.jsx
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -6,10 +6,12 @@ const ActiveQuiz = props => (
   <div className={s.ActiveQuiz}>
     <p className={s.Question}>
       <span>
-        <strong>2.</strong>&nbsp;{props.question}
+        <strong>{props.answerNumber}.</strong>&nbsp;{props.question}
       </span>
 
-      <small>4 из 12</small>
+      <small>
+        {props.answerNumber} из {props.quizLength}
+      </small>
     </p>
 
     <AnswersList answers={props.answers} onAnswerClick={props.onAnswerClick} />
--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -4,10 +4,12 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 
 class Quiz extends Component {
   state = {
+    activeQuestion: 0,
     quiz: [
       {
         question: 'Какого цвета небо?',
         rightAnswer: 2,
+        id: 1,
         answers: [
           { text: 'Черный', id: 1 },
           { text: 'Синий', id: 2 },
@@ -15,10 +17,22 @@ class Quiz extends Component {
           { text: 'Зелёный', id: 4 },
         ],
       },
+      {
+        question: 'Мой год рождения?',
+        rightAnswer: 1,
+        id: 2,
+        answers: [
+          { text: '1983', id: 1 },
+          { text: '1993', id: 2 },
+          { text: '1989', id: 3 },
+          { text: '2000', id: 4 },
+        ],
+      },
     ],
   };
   onAnswerClickHandler = answerId => {
     console.log('answerId', answerId);
+    this.setState({ activeQuestion: this.state.activeQuestion + 1 }); //меняем на сл.вопрос при ответе на вопрос
   };
   render() {
     return (
@@ -27,9 +41,11 @@ class Quiz extends Component {
           <h1>Ответьте на все вопросы</h1>
 
           <ActiveQuiz
-            answers={this.state.quiz[0].answers}
-            question={this.state.quiz[0].question}
+            answers={this.state.quiz[this.state.activeQuestion].answers} //показывает номер вопроса
+            question={this.state.quiz[this.state.activeQuestion].question} //показывает номер вопроса
             onAnswerClick={this.onAnswerClickHandler}
+            quizLength={this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1} //отображение для пользователя(индекс вопроса начинается с 0) делаем +1
           />
         </div>
       </div>
