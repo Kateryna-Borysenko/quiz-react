a444ee5949f6eb0bff505400347eab6f1c3801f8
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-12-25 15:02:54 +0200

    Added validation of Email and Password

--- src/containers/Quiz/Auth/Auth.jsx
+++ src/containers/Quiz/Auth/Auth.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import s from './Auth.module.css';
 import Button from '../../../components/ActiveQuiz/UI/Button/Button';
 import Input from '../../../components/ActiveQuiz/UI/Button/Input/Input';
+import is from 'is_js';
+
+//если использовать regex
+// const validateEmail = email => {
+//   const re =
+//     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// };
 
 export class Auth extends Component {
   state = {
@@ -43,9 +51,43 @@ export class Auth extends Component {
   submitHandler = e => {
     e.preventDefault();
   };
+  validateControl(value, validation) {
+    //обозначает что если не передали параметры валидации то валидировать control нам не нужно
+    if (!validation) {
+      return true;
+    }
+    //если validation есть
+    let isValid = true;
+    //если поле не пустое и до этого isValid не было false (в нашем случае можно не писать, но для универсальности ф-ции)
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    //is js библиотека которая проверяет на валидацию email или мужно использовать регулярное выражение
+    if (validation.email) {
+      isValid = is.email(value) && isValid;
 
-  onChangeHandler = (event, controlName) => {
-    console.log(`${controlName}: `, event.target.value);
+      //2й вариант с regex
+      // isValid = validateEmail(value) && isValid;
+    }
+    //если строка больше или равна укажаному символу
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+  }
+
+  onChangeHandler = (e, controlName) => {
+    console.log(`${controlName}: `, e.target.value);
+    //чтобы не мутировать объект создаём копию
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] }; //либо email либо password со всеми полями
+    //дальше просто переопределяем безопасно без мутации поля
+    control.value = e.target.value; //считываем значение введенное в инпут
+    control.touched = true; //как только пользователь что-то ввел значение становиться true
+    control.valid = this.validateControl(control.value, control.validation); //новое значение введенное и объект валидации код 18 32
+    formControls[controlName] = control;
+    this.setState({ formControls });
   };
 
   renderInput = () => {
