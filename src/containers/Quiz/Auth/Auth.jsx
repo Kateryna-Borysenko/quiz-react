69c1583ab2db3df9c640baf5705c20502edc2c7c
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-12-25 13:21:18 +0200

    Added castom input component

--- src/containers/Quiz/Auth/Auth.jsx
+++ src/containers/Quiz/Auth/Auth.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import s from './Auth.module.css';
+import Button from '../../../components/ActiveQuiz/UI/Button/Button';
+import Input from '../../../components/ActiveQuiz/UI/Button/Input/Input';
 
 export class Auth extends Component {
+  loginHadler = () => {};
+
+  registerHadler = () => {};
+
+  submitHandler = e => {
+    e.preventDefault();
+  };
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={s.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form className={s.AuthForm} onSubmit={this.submitHandler}>
+            <Input label="Email" />
+            <Input label="Пароль" errorMessage={'TEST'} />
+            <Button type="success" onClick={this.loginHadler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHadler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
