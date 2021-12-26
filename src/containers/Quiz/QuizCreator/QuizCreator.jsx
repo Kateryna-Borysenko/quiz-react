7275219ef49dfa35be4de0712bc25f9552a95b36
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-12-26 13:11:37 +0200

    added form for creating test

--- src/containers/Quiz/QuizCreator/QuizCreator.jsx
+++ src/containers/Quiz/QuizCreator/QuizCreator.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import s from './QuizCreator.module.css';
+import Button from '../../../components/ActiveQuiz/UI/Button/Button';
+import Input from '../../../components/ActiveQuiz/UI/Button/Input/Input';
 
 export class QuizCreator extends Component {
+  onSubmitHandler = e => {
+    e.preventDefault();
+  };
+  addQuestionHandler = () => {};
+  createQuizHandler = () => {};
   render() {
     return (
-      <div>
-        <h1>Quiz Creator</h1>
+      <div className={s.QuizCreator}>
+        <div>
+          <h1>Создание теста</h1>
+          <form onSubmit={this.onSubmitHandler}>
+            <input type="text" />
+            <hr />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <select name="" id=""></select>
+            <Button type="primary" onClick={this.addQuestionHandler}>
+              Добавить вопрос
+            </Button>
+            <Button type="success" onClick={this.createQuizHandler}>
+              Создать тест
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
