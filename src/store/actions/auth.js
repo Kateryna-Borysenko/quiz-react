bcf8c812556deaf4c7cd904b9b0eeb4c17cc8773
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2022-01-02 14:17:40 +0200

    Added auth - redux

---
+++ src/store/actions/auth.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { AUTH_LOGOUT, AUTH_SUCCESS } from './actionTypes';
+
+export function auth(email, password, isLogin) {
+  return async dispatch => {
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true,
+    };
+
+    let url =
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBHGAzz22mPCTQnJHHkVoC84ahlL0gSTgk';
+
+    if (isLogin) {
+      url =
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBHGAzz22mPCTQnJHHkVoC84ahlL0gSTgk';
+    }
+
+    const response = await axios.post(url, authData);
+    const data = response.data;
+    // console.log(data);
+
+    //истичение срока - через 1ч закончится наша сессия
+    const expirationDate = new Date(
+      new Date().getTime() + data.expiresIn * 1000,
+    );
+
+    localStorage.setItem('token', data.idToken);
+    localStorage.setItem('userId', data.localId);
+    localStorage.setItem('expirationDate', expirationDate);
+
+    dispatch(authSuccess(data.idToken));
+    dispatch(autoLogout(data.expiresIn)); //экспаирс ин -> истикает
+  };
+}
+
+export function autoLogout(time) {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, time * 1000);
+  };
+}
+
+export function logout() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('userId');
+  localStorage.removeItem('expirationDate');
+  return {
+    type: AUTH_LOGOUT,
+  };
+}
+
+export function authSuccess(token) {
+  return {
+    type: AUTH_SUCCESS,
+    token,
+  };
+}
